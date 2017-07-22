89e515f19b75918a7773391d89614e36d61259c4
Victor Villas <villasv@outlook.com>
2017-07-22 19:44:12 -0300

    prettier donut

--- Sandbox/static/scripts/src/dashboard/dashboard.js
+++ Sandbox/static/scripts/src/dashboard/dashboard.js
@@ -10,20 +10,84 @@ angular.module('app.dashboard', ['ngRoute', 'highcharts-ng'])
 
     $scope.username = "Carolina";
 
-    // Porftolio 
+    // Data 
     $scope.portfolio = []
     $http.get('/portfolio')
         .then(function (response) {
             $scope.portfolio = response.data.portfolio;
-
-            // Update Stuff
-            updateTreasuryDonut($scope.portfolio);
+            updateTreasuryDonut();
         });
     
+    // Data Transforms
+    $scope.getPortifolioTotalValue = function () {
+        total = 0.0;
+        for (i = 0; i < $scope.portfolio.length; i += 1) {
+            total += $scope.portfolio[i].value;
+        }
+        return total;
+    }
+    
     // Treasury Donut
-    function updateTreasuryDonut(portfolio) {
+    function updateTreasuryDonut() {
         $scope.treasuryDonutConfig.series[0].data = aggregate($scope.portfolio);
         $scope.treasuryDonutConfig.series[1].data = drilldown($scope.portfolio);
+    }
+    function getTreasuryColor(name) {
+        if (~name.indexOf('LTN')) {
+            return '#fe9929';
+        }
+        if (~name.indexOf('LFT')) {
+            return '#fed98e';
+        }
+        if (~name.indexOf('NTNB Princ')) {
+            return '#ec7014';
+        }
+        if (~name.indexOf('NTNB')) {
+            return '#cc4c02';
+        }
+        if (~name.indexOf('NTNF')) {
+            return '#8c2d04';
+        }
+    }
+    function aggregate(portfolio) {
+        categories = ['LTN', 'LFT', 'NTNB', 'NTNB Princ', 'NTNF'];
+        total = $scope.getPortifolioTotalValue();
+
+        data = [];
+        for (i = 0; i < categories.length; i += 1) {
+            assets = portfolio.filter(function (asset) {return ~asset.name.indexOf(categories[i])});
+            subtotal = 0.0;
+            for (j = 0; j < assets.length; j += 1) {
+                subtotal += assets[j].value;
+            }
+            data.push({
+                name: categories[i],
+                y: (subtotal / total),
+                color: getTreasuryColor(categories[i])
+            });
+        }
+        return data
+    }
+    function drilldown(portfolio) {
+        categories = ['LTN', 'LFT', 'NTNB', 'NTNB Princ', 'NTNF'];
+        total = $scope.getPortifolioTotalValue();
+
+        ordered_portfolio = [];
+        for (i = 0; i < categories.length; i += 1) {
+            ordered_portfolio = ordered_portfolio.concat(
+                portfolio.filter(function (asset) {return ~asset.name.indexOf(categories[i])})
+            );
+        }
+
+        data = []
+        for (i = 0; i < ordered_portfolio.length; i += 1) {
+            data.push({
+                name: ordered_portfolio[i].name,
+                y: (ordered_portfolio[i].value / total),
+                color: getTreasuryColor(ordered_portfolio[i].name)
+            })
+        }
+        return data
     }
     $scope.treasuryDonutConfig = {
         chart: {
@@ -88,57 +152,3 @@ angular.module('app.dashboard', ['ngRoute', 'highcharts-ng'])
         componentHandler.upgradeDom();
     });
 });
-
-// Treasury Donut
-function getTreasuryColor(name) {
-    if (~name.indexOf('LTN')) {
-        return '#fe9929';
-    }
-    if (~name.indexOf('LFT')) {
-        return '#fed98e';
-    }
-    if (~name.indexOf('NTNB Princ')) {
-        return '#ec7014';
-    }
-    if (~name.indexOf('NTNB')) {
-        return '#cc4c02';
-    }
-    if (~name.indexOf('NTNF')) {
-        return '#8c2d04';
-    }
-}
-function aggregate(portfolio) {
-    categories = ['LTN', 'LFT', 'NTBN', 'NTNB Princ', 'NTNF'];
-    data = [];
-    for (i = 0; i < categories.length; i += 1) {
-        assets = portfolio.filter(function (asset) {return ~asset.name.indexOf(categories[i])});
-        total = 0.0;
-        for (j = 0; j < assets.length; j += 1) {
-            total += assets[j].value;
-        }
-        data.push({
-            name: categories[i],
-            y: total,
-            color: getTreasuryColor(categories[i])
-        });
-    }
-    return data
-}
-function drilldown(portfolio) {
-    categories = ['LTN', 'LFT', 'NTBN', 'NTNB Princ', 'NTNF'];
-    ordered_portfolio = [];
-    for (i = 0; i < categories.length; i += 1) {
-        ordered_portfolio = ordered_portfolio.concat(
-            portfolio.filter(function (asset) {return ~asset.name.indexOf(categories[i])})
-        );
-    }
-    data = []
-    for (i = 0; i < ordered_portfolio.length; i += 1) {
-        data.push({
-            name: ordered_portfolio[i].name,
-            y: ordered_portfolio[i].value,
-            color: getTreasuryColor(ordered_portfolio[i].name)
-        })
-    }
-    return data
-}
