9a6629ccfa2ce5c8b57d8f329b7354a925adf04d
Lucas Meireles <meireles31@msn.com>
2017-07-22 09:27:32 -0300

    changed navbar to be a new component

--- Sandbox/static/scripts/src/navbar/navbar.js
+++ Sandbox/static/scripts/src/navbar/navbar.js
@@ -5,7 +5,48 @@ function navBarController($scope) {
     $scope.headerList = [
         {
             text: "Home",
-            url: "#!/"
+            url: "#!/",
+            iClass: "home"
+        },
+        {
+            text: "Inbox",
+            url: "#!/",
+            iClass: "inbox"
+        },
+        {
+            text: "Trash",
+            url: "#!/",
+            iClass: "delete"
+        },
+        {
+            text: "Spam",
+            url: "#!/",
+            iClass: "report"
+        },
+        {
+            text: "Forums",
+            url: "#!/",
+            iClass: "forum"
+        },
+        {
+            text: "Updates",
+            url: "#!/",
+            iClass: "flag"
+        },
+        {
+            text: "Promos",
+            url: "#!/",
+            iClass: "local_offer"
+        },
+        {
+            text: "Purchases",
+            url: "#!/",
+            iClass: "shopping_cart"
+        },
+        {
+            text: "Social",
+            url: "#!/",
+            iClass: "people"
         },
     ];
 }
