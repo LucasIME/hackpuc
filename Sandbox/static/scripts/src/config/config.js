dc5d003203efaed775c3710d7644a3127c2fbc0e
Lucas Meireles <meireles31@msn.com>
2017-07-23 02:32:32 -0300

    Replicated dashboard changes to config page.

--- Sandbox/static/scripts/src/config/config.js
+++ Sandbox/static/scripts/src/config/config.js
@@ -8,6 +8,18 @@ angular.module('app.config', ['ngRoute'])
     $scope.username = "Carolina";
     $scope.savePercentage = 0;
 
+    // Data 
+    $scope.balance = 0;
+    $http.get('/balance')
+        .then(function (response) {
+            $scope.balance = response.data.balance;
+        });
+    $scope.projected = 0;
+    $http.get('/projected')
+        .then(function (response) {
+            $scope.projected = response.data.projected;
+        });
+
     angular.element(document).ready(function(){
         componentHandler.upgradeDom();
     });
